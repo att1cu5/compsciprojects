bd0555f3d8466ac8ff984dcfb001f3ad8064fcd7
att1cu5 <atticusmgr27@rdcrs.ca>
2026-02-05 13:09:47 -0700

    Create functionparameter.py

---
+++ functionparameter.py
@@ -0,0 +1,15 @@
+print("hi my name is bot what is yours")
+name=input()
+print("hi ",name)
+print("number one:")
+i=int(input())
+print("number two:")
+k=int(input())
+def h(l,o):
+  return l*o
+def r(s,d):
+  return s+d
+if((i*k)<=1000):
+  print(h(i,k))
+else:
+  print(r(i,k))
